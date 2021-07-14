12f446b0b6e125da171d46b5ab2be8cda4cb1acb
jhs512 <jangka512@gmail.com>
2021-07-14 16:46:43 +0900

    변수와 if 문

--- docs/test.php
+++ docs/test.php
@@ -1,6 +1,11 @@
 <?php
-$name = "홍길순";
-$age = 24;
+$name = $_GET['name'];
+$age = $_GET['age'];
+
+if ( $age < 20 ) {
+  echo "denied!";
+  exit;
+}
 ?>
 <meta charset="UTF-8">
 안녕하세요. 저는 <?=$name?>, <?=$age?>살입니다.
