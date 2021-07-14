e8e2599b1123d9a8bab270333e4d3762989f07e0
jhs512 <jangka512@gmail.com>
2021-07-14 17:25:56 +0900

    test2.php 추가

---
+++ docs/test2.php
@@ -0,0 +1,5 @@
+<?php echo "<div style='border:10px solid red; padding:10px;'></div>"; ?>
+
+<?="<div style='border:10px solid red; padding:10px;'></div>"?>
+
+<div style='border:10px solid red; padding:10px;'></div>
